a81e027ec2880463cbeca01a1b2928232c873a31
DonDotDon Studio <dondotdon.studio@gmail.com>
2026-02-08 00:01:40 +0900

    Add files via upload

--- devlog-data.js
+++ devlog-data.js
@@ -2,7 +2,7 @@ const devLogs = [
   {
     project: "Underground House",
     date: "2026.02.07",
-    title: "게임 내에 세계관 논의",
-    summary: "스토리, 주인공, 컨셉아트 논의중."
+    title: "UH Project 첫 회의",
+    summary: "게임 세계관 확립, 컨셉아트 기획, 캐릭터 기본 UI 기획"
   }
 ];
